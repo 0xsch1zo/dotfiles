61d7976ea5fceb84ea44e13327e2116ba793ff27
sentientbottleofwine <sentientbottleofwine@gmail.com>
2025-01-03 21:20:25 +0100

    Add set some useful settings

--- .config/nvim/lua/config/misc.lua
+++ .config/nvim/lua/config/misc.lua
@@ -23,3 +23,9 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", ";", ":")
+
+vim.o.wrap = false
+vim.o.incsearch = true
+vim.o.termguicolors = true
+vim.o.scrolloff = 8
+vim.o.updatetime = 50
