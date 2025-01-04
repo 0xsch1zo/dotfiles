55dea70c984c19ffeaa14ea58e099aaca9a541a5
sentientbottleofwine <sentientbottleofwine@gmail.com>
2025-01-04 12:57:48 +0100

    Include hidden files in neo-tree and telescope

--- .config/nvim/lua/config/neo-tree.lua
+++ .config/nvim/lua/config/neo-tree.lua
@@ -1,1 +1,10 @@
+require("neo-tree").setup({
+    filesystem = {
+        filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = true
+        }
+    }
+})
 vim.keymap.set('n', '<C-n>', ':Neotree position=current<CR>', {})
--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -4,10 +4,15 @@ vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
 
 require("telescope").setup {
-  extensions = {
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown {}
+    pickers = {
+        find_files = {
+            hidden = true
+        }
+    },
+    extensions = {
+        ["ui-select"] = {
+            require("telescope.themes").get_dropdown {}
+        }
     }
-  }
 }
 require("telescope").load_extension("ui-select")
