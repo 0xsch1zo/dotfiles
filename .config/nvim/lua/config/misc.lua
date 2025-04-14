16e72d7b892ce5c3b131abaab22ac9eb72939286
sentientbottleofwine <sentientbottleofwine@gmail.com>
2025-04-14 22:18:38 +0200

    Turn on relative line numbers in vim. Switch to zen as default.

--- .config/nvim/lua/config/misc.lua
+++ .config/nvim/lua/config/misc.lua
@@ -3,7 +3,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
-vim.cmd("set number")
+vim.cmd("set relativenumber")
 
 -- Clipboard
 vim.opt.clipboard = "unnamedplus"
