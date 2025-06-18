bfd2948a54c3d205c6af83799a37a01e0d951e06
sentientbottleofwine <sentientbottleofwine@gmail.com>
2025-06-18 19:30:12 +0200

    Last keybind change I swear

--- .config/nvim/lua/config/misc.lua
+++ .config/nvim/lua/config/misc.lua
@@ -11,8 +11,8 @@ vim.opt.clipboard = "unnamedplus"
 -- Some keybinds
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '>-2<CR>gv=gv")
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-h>', '<C-u>zz')
+vim.keymap.set('n', '<C-s>', '<C-d>zz')
+vim.keymap.set('n', '<C-d>', '<C-u>zz')
 vim.keymap.set('n', "n", "nzzzv")
 vim.keymap.set('n', "N", "Nzzzv")
 vim.keymap.set('x', "<leader>p", '"_dP')
